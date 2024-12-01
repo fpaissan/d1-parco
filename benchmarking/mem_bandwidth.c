f210dd8973ee6ec28ae8d110a7b603ffa4f61bd5
Francesco Paissan <46992226+fpaissan@users.noreply.github.com>
2024-12-01 09:03:19 +0100

    polishing

--- benchmarking/mem_bandwidth.c
+++ benchmarking/mem_bandwidth.c
@@ -3,7 +3,7 @@
  *
  * Author: Francesco Paissan, 2024
  *
- * Compile with gcc -o bench -O3 -march=native mem_bandwidth.c 
+ * Compile with gcc -o bench -O3 -march=native mem_bandwidth.c
  */
 #include<stdio.h>
 #include<stdlib.h>
@@ -13,36 +13,36 @@
 #define AVG_NUMBER 1000
 #define SIZE (unsigned long) pow(2, 20)
 
-int main(){
+int main() {
 
-  int *array = (int*)malloc(sizeof(int) * SIZE);
-  if(array == NULL){
-    printf("Memory allocation failed.\n");
-    return 1;
-  }
-
-  double cpu_time_used;
-  clock_t start, end;
-
-  for(int i=0; i<AVG_NUMBER; i++){
-    start = clock();
-    for ( long i = 0; i < SIZE ; i++) {
-      array[i] = i ;
+    int *array = (int*)malloc(sizeof(int) * SIZE);
+    if(array == NULL) {
+        printf("Memory allocation failed.\n");
+        return 1;
     }
-  
-    for ( long i = 0; i < SIZE ; i++) {
-      int value = array [ i ];
+
+    double cpu_time_used;
+    clock_t start, end;
+
+    for(int i=0; i<AVG_NUMBER; i++) {
+        start = clock();
+        for ( long i = 0; i < SIZE ; i++) {
+            array[i] = i ;
+        }
+
+        for ( long i = 0; i < SIZE ; i++) {
+            int value = array [ i ];
+        }
+        end = clock();
+
+        cpu_time_used += (double)(end - start) / CLOCKS_PER_SEC / AVG_NUMBER;
     }
-    end = clock();
-  
-    cpu_time_used += (double)(end - start) / CLOCKS_PER_SEC / AVG_NUMBER;
-  }
 
 
-  double moved = 2.0 * sizeof(int) * SIZE;
-  double rate = moved / (cpu_time_used * 1e9);
+    double moved = 2.0 * sizeof(int) * SIZE;
+    double rate = moved / (cpu_time_used * 1e9);
 
-  printf("It took %.6f s to move %f B.\n", cpu_time_used, moved);
-  printf("The rate is %f GB/s.\n", rate);
+    printf("It took %.6f s to move %f B.\n", cpu_time_used, moved);
+    printf("The rate is %f GB/s.\n", rate);
 
 }
